47c3af8b4583198db1600d0880c08ec7c25b4816
Linus7671TV <bubiks090@gmail.com>
2026-07-22 18:37:45 +0300

    Add set_rds_lic function declaration in rds.h

--- src/pifmrds/rds.h
+++ src/pifmrds/rds.h
@@ -50,6 +50,61 @@ extern void set_rds_tp(int tp);
 extern void set_rds_ms(int ms);
 extern void set_rds_ab(int ab);
 extern void set_rds_ecc(uint16_t ecc);
+extern void set_rds_lic(uint16_t lic);
+extern void set_rds_ptyn(char *ptyn);
+
+#endif /* RDS_H *//*
+    PiFmRds - FM/RDS transmitter for the Raspberry Pi
+    Copyright (C) 2014 Christophe Jacquet, F8FTK
+    
+    See https://github.com/ChristopheJacquet/PiFmRds
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef RDS_H
+#define RDS_H
+
+#define RT_LENGTH 64
+#define PS_LENGTH 8
+#define GROUP_LENGTH 4
+
+#define POLY 0x1B9
+#define POLY_DEG 10
+#define MSB_BIT 0x8000
+#define BLOCK_SIZE 16
+
+#define BITS_PER_GROUP (GROUP_LENGTH * (BLOCK_SIZE+POLY_DEG))
+#define SAMPLES_PER_BIT 192
+#define FILTER_SIZE (sizeof(waveform_biphase)/sizeof(float))
+#define SAMPLE_BUFFER_SIZE (SAMPLES_PER_BIT + FILTER_SIZE)
+
+#include <stdint.h>
+
+extern void get_rds_samples(float *buffer, int count, int stereo, int ct_clock_enabled, float sample_volume);
+extern void get_rds_group(int *buffer, int stereo, int ct_clock_enabled);
+extern void set_rds_pi(uint16_t pi_code);
+extern void set_rds_rt(char *rt);
+extern void set_rds_ps(char *ps);
+extern void set_rds_ta(int ta);
+extern void set_rds_pty(int pty);
+extern void set_rds_di(int di);
+extern void set_rds_af(int *af_array);
+extern void set_rds_tp(int tp);
+extern void set_rds_ms(int ms);
+extern void set_rds_ab(int ab);
+extern void set_rds_ecc(uint16_t ecc);
 extern void set_rds_ptyn(char *ptyn);
 
 #endif /* RDS_H */
